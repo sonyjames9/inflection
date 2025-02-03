593ebd84ae2d8da2de5baabd159ada7367993603
sj9_y_mac <sonyjames9@yahoo.com>
2025-02-04 01:45:41 +0530

    return response and status code

--- campaign/base_api.py
+++ campaign/base_api.py
@@ -16,22 +16,21 @@ class BaseAPI:
         logger.info(f"GET {url}")
         response = self.session.get(url)
         self._log_response(response)
-        return response.json()
-    
+        return response.json(), response.status_code
+
     def post(self, endpoint, data):
         url = f"{self.base_url}{endpoint}"
         logger.info(f"POST {url} - Payload: {data}")
         response =  self.session.post(url, json=data)
-        self._log_response(response)
-        return response.json()
-    
+        return response.json(), response.status_code
+
     def patch(self, endpoint, data):
         url = f"{self.base_url}{endpoint}"
         logger.info(f"PATCH {url} - Payload: {data}")
         response =  self.session.patch(url, json=data)
         self._log_response(response)
-        return response.json()
-    
+        return response.json(), response.status_code
+
     def _log_response(self, response):
         logger.info(f"Response Code: {response.status_code}")
         logger.info(f"Response Body: {response.text}")
